a05149b420b92a318f1f394544448090e0a51d0f
Patrik Lythell <patrik@skrivapa.se>
2013-01-10 23:23:27 +0000

    fix for help and colors and readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Dependent
+
+Lazy dependency script for lazy developers compiling your node modules to a depencies.json
+
+## Requirements
+
+Node JS and NPM installed.
+Only been tested in MacOSX (hey it's for lazy developers by a lazy developer, what can I say?)
+
+## Installation
+
+Coming soon to NPM…
+
+## Usage
+
+After installing globally run 'dependent' in your working dir. This will look up what node modules can be found inside node_modules and compile them into a dependencies.json in your working dir. Copy and paste into your package.json.
+
+## Changelog
+
+##### 0.0.1
+* Initial Release
--- dependent.js
+++ dependent.js
@@ -1,18 +1,28 @@
 #! /usr/bin/env node
 
+//Dependencies
 var fs = require('fs');
-var colors = require('colors');
+
+//terminal output colours!
+//via http://roguejs.com/2011-11-30/console-colors-in-node-js/
+var red, blue, reset;
+red   = '\u001b[31m';
+blue  = '\u001b[34m';
+reset = '\u001b[0m';
 
 var home = process.env.PWD + '/node_modules/';
 
 var installedDeps = [];
 
+//Function to see if this is a dir
 isDirectory = function(item, callback) {
   fs.stat(home + item, function(err, stats) {
     if (err) {
       throw err;
     }
-    if (stats.isDirectory()) {
+    //Fix for ignoring dirs such as ./bin
+    dotCheck = item.slice(0,1)
+    if (stats.isDirectory() && dotCheck !== '.') {
       installedDeps.push(home + item);
     }
     return callback();
@@ -21,6 +31,17 @@ isDirectory = function(item, callback) {
 
 dependencies = {};
 
+//Writing depencies.json to PWD
+writeDependencies = function(object) {
+  fs.writeFile(process.env.PWD + '/dependencies.json', JSON.stringify(object, null, 2) + ',', 'utf-8', function(err, data) {
+    if (err) {
+      console.log(red + "Something went wrong!" + reset);
+    }
+    return console.log(blue + "Created dependencies.json " + reset + "in " + blue + process.env.PWD + reset);
+  });
+};
+
+//function for reading package.json from modules
 readPackage = function(item, callback) {
   var data, name, version;
   data = require(item + '/package.json');
@@ -30,15 +51,7 @@ readPackage = function(item, callback) {
   return callback();
 };
 
-writeDependencies = function(object) {
-  fs.writeFile(process.env.PWD + '/dependencies.json', JSON.stringify(object, null, 2) + ',', 'utf-8', function(err, data) {
-    if (err) {
-      console.log("Something went wrong!".red);
-    }
-    return console.log("Written dependencies.json to ".cyan + process.env.PWD.green);
-  });
-};
-
+//parent function for reading package.json 
 getNameAndVersion = function(modules) {
   var counter = 0;
   modules.forEach(function(module){
@@ -51,6 +64,7 @@ getNameAndVersion = function(modules) {
   });
 };
 
+//init reading dirs in node_modules
 init = function() {
   fs.readdir(home, function(err, data) {
     var counter = 0;
@@ -71,12 +85,23 @@ init = function() {
 var userArgs = process.argv.slice(2);
 var userInput = userArgs[0];
 if (userInput) {
-  console.log(userInput);
   var input = userInput.toLowerCase();
   if ( input === '-v' || input === '--version' ) {
-    console.log('v0.1.0');
+    console.log('v0.0.1');
   } else {
-    console.log('help');
+    console.log("-> " + red + "Dependent " + reset + "-" + red + " v0.0.1" + reset);
+    console.log("-> To upgrade to latest version: npm update dependent -g");
+    console.log("");
+    console.log("-> USAGE: 'dependent'");
+    console.log("");
+    console.log("-> BASIC USAGE");
+    console.log("---> Run dependent in your working directory and a dependencies.json will be");
+    console.log("---> created of all your node_modules.");
+    console.log("");
+    console.log("-> any feedback, help or issues, please report them on");
+    console.log("Github: https://github.com/patriklythell/dependent/");
+    console.log("");
+    process.exit(1);
   } 
 } else {
   init();
